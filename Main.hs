acde3cb0cb9a27de9d7b7cdf60af3fa971861a96
phischu <pschuster@uni-koblenz.de>
2014-01-09 14:50:13 +0100

    encode and decode images

--- Main.hs
+++ Main.hs
@@ -1,11 +1,9 @@
 module Main where
 
+import Codec.Picture.Repa
 import Codec.Picture
-import Codec.Picture.Types
 import Data.Array.Repa
 import Data.Array.Repa as Repa
-import Data.Array.Repa.Repr.Vector as Repa
-import Data.Vector as Vector
 
 
 
@@ -15,27 +13,21 @@ data Blob a = Blob a a a deriving (Show)
 blob :: Array r DIM2 Double -> Blob Double -> Blob Double
 blob _ _ = Blob 1 2 3
 
-loadGrayImage :: FilePath -> IO (Either String (Array D DIM2 Double))
-loadGrayImage filepath = do
-	eitherdynamicimage <- readImage filepath
-	return (eitherdynamicimage >>= extractLuma >>= return . imageToArray >>= return . (Repa.map pixel8ToDouble))
+imageToArray :: Img RGB -> Array D DIM2 Double
+imageToArray = Repa.map (/255) . Repa.sumS . Repa.map fromIntegral . imgData
 
-extractLuma :: DynamicImage -> Either String (Image Pixel8)
-extractLuma (ImageRGB8 image) = Right (extractLumaPlane image)
-extractLuma (ImageRGBA8 image) = Right (extractLumaPlane image)
-extractLuma (ImageYCbCr8 image) = Right (extractLumaPlane image)
-extractLuma _ = Left "Image not RGB8 or RGBA8 or YCbCr8"
-
-imageToArray :: Image Pixel8 -> Array V DIM2 Pixel8
-imageToArray (Image w h d) = Repa.fromVector (Z:.w:.h) (Vector.convert d)
-
-pixel8ToDouble :: Pixel8 -> Double
-pixel8ToDouble pixel = fromIntegral pixel / 255
+arrayToImage :: Array D DIM2 Double -> Image Pixel8
+arrayToImage arr = generateImage (\x y -> pixelArray ! (Z:.y:.x)) w h where
+    pixelArray = Repa.map truncate (Repa.map ((*) (255/elementRange)) (Repa.map (+minimumElement) arr))
+    minimumElement = foldAllS min (1/0) arr
+    maximumElement = foldAllS max (-1/0) arr
+    elementRange = maximumElement - minimumElement
+    Z:.h:.w = extent arr
 
 main :: IO ()
 main = do
-	loadGrayImage "sunflowers.jpg" >>= either print (\sunflowers -> do
-		print (blob sunflowers (Blob 12 45 5))) 
+    readImageRGB "sunflowers.jpg" >>= either print (\sunflowers -> do
+        writePng "result.png" (arrayToImage (imageToArray sunflowers)))
 
 
 
