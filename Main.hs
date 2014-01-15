12a51b8e5d9559cacc42a5483226a915ea00ed00
phischu <pschuster@uni-koblenz.de>
2014-01-15 18:27:36 +0100

    laplacian of gaussian

--- Main.hs
+++ Main.hs
@@ -1,20 +1,43 @@
+{-# LANGUAGE DeriveFunctor,DeriveFoldable,DeriveTraversable #-}
 module Main where
 
 import Codec.Picture.Repa
 import Codec.Picture
-import Data.Array.Repa hiding ((++))
+import Data.Array.Repa hiding ((++),map)
 import qualified Data.Array.Repa as Repa
+import Numeric.AD
+import Numeric.AD.Types
+import Data.Traversable (Traversable)
+import Data.Foldable (Foldable)
 
 
 
-data Blob a = Blob a a a deriving (Show)
+data Blob a = Blob a a a deriving (Show,Functor,Foldable,Traversable)
 
 
-blob :: Array r DIM2 Double -> Blob Double -> Blob Double
-blob _ _ = Blob 100 40 33
+blob :: Array D DIM2 Double -> Blob Double -> [Blob Double]
+blob a b = gradientAscent (blobbiness (toRelation a)) b
+
+blobbiness :: (Mode m) => Relation (AD m Double) -> Blob (AD m Double) -> AD m Double
+blobbiness r b = apply (blobmask b) r
+
+toRelation :: (Mode m) => Array D DIM2 Double -> [((AD m Double,AD m Double),AD m Double)]
+toRelation arr = Repa.toList (Repa.traverse arr id (\a (Z:.i:.j) -> ((fromIntegral i,fromIntegral j),auto (a (Z:.i:.j)))))
+
+type Relation a = [((a,a),a)]
+type Mask a = (a,a) -> a
+
+apply :: (Num a) => Mask a -> Relation a -> a
+apply mask image = sum [mask x * v | (x,v) <- image]
+
+blobmask :: (Num a,Floating a,Fractional a) => Blob a -> Mask a
+blobmask (Blob x y t) (i,j) = - t * (1/t^4) * (1 / (2*pi)) * (((x-i)^2 + (y-j)^2)/t^2 - 2) * exp ((-1/2) * ((x-i)^2 + (y-j)^2)/t^2)
+
+laplacianOfGaussian :: (Num a,Fractional a,Floating a) => a -> a -> a -> a -> a -> a
+laplacianOfGaussian x y t i j = - t * (1/t^4) * (1 / (2*pi)) * (((x-i)^2 + (y-j)^2)/t^2 - 2) * exp ((-1/2) * ((x-i)^2 + (y-j)^2)/t^2)
 
 imageToArray :: Img RGB -> Array D DIM2 Double
-imageToArray = Repa.map (/255) . Repa.sumS . Repa.map fromIntegral . imgData
+imageToArray = Repa.map (/(255*3)) . Repa.sumS . Repa.map fromIntegral . imgData
 
 arrayToImage :: Array D DIM2 Pixel8 -> Image Pixel8
 arrayToImage arr = generateImage (\x y -> arr ! (Z:.y:.x)) w h where
@@ -28,20 +51,21 @@ doubleArrayToPixel8Array arr = Repa.map truncate (Repa.map ((*) (255/elementRang
 
 main :: IO ()
 main = do
-    readImageRGB "sunflowers.jpg" >>= either print (\sunflowers -> do
+    readImageRGB "squaredirty.png" >>= either print (\sunflowers -> do
         let array = imageToArray sunflowers
-            b = blob array (Blob 23 24 25)
+            b = take 50 (blob array (Blob 11 12 3))
             pixelArray = doubleArrayToPixel8Array array
-            result = renderBlob pixelArray b
+            result = renderBlob pixelArray (last b)
             image = arrayToImage result
+        putStrLn (unlines (map show b))
         writePng "result.png" image)
 
 renderBlob :: Array D DIM2 Pixel8 -> Blob Double -> Array D DIM2 Pixel8
-renderBlob arr blob = traverse arr id (\a i -> if i `elem` outline blob then 255 else a i)
+renderBlob arr b = traverse arr id (\a i -> if i `elem` outline b then 255 else a i)
 
 outline :: Blob Double -> [DIM2]
 outline (Blob x y t) =
-    let t' = truncate (0.5 * t)
+    let t' = truncate t
         x' = truncate x
         y' = truncate y in
     [Z:.i:.j | let i = y'+t', j <- [x'-t' .. x'+t'] ] ++
